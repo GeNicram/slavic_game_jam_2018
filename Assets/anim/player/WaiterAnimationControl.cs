3a27c55c67c07a46d0e0da51cbca05ec3d22cee6
Wysocki Patryk <wysockipawelpatryk@gmail.com>
2018-07-20 22:39:53 +0200

    Base for animator added

---
+++ Assets/anim/player/WaiterAnimationControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaiterAnimationControl : MonoBehaviour {
+
+	public Waiter waiter;
+	public Animator animator;
+	public Rigidbody2D body;
+
+	// Use this for initialization
+	void Start () {
+		waiter = GetComponent<Waiter>();
+		animator = GetComponent<Animator>();
+		body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animator.SetFloat("speed", body.velocity.magnitude);
+	}
+}
