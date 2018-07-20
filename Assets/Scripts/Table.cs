b6ed411c1eab06f388a4e5b16bd8f9002edabf33
Andreas Vennström <andreas.vennstrom@gmail.com>
2018-07-20 19:17:43 +0200

    Add tables https://youtu.be/adyC404_gEk

---
+++ Assets/Scripts/Table.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Table : MonoBehaviour
+{
+    const uint dishTypeCount = 5;
+
+    private uint m_tip = 0;
+    private uint m_desiredDishType = 0;
+    private bool m_isWaitingForDish = false;
+
+    public uint desiredDishType
+    {
+        get { return m_desiredDishType; }
+        set { m_desiredDishType = value; }
+    }
+
+    public bool isWaitingForDish
+    {
+        get { return m_isWaitingForDish; }
+    }
+
+    private void Start()
+    {
+        m_tip = 0;
+        desiredDishType = (uint)Random.Range(0, dishTypeCount - 1);
+        m_isWaitingForDish = true;
+
+    }
+    
+    public void LeaveTip(uint tip)
+    {
+        m_tip += tip;
+    }
+
+    public uint TakeTip()
+    {
+        uint tip = m_tip;
+        m_tip = 0;
+        return tip;
+    }
+
+    public bool Serve(uint dishType)
+    {
+        if (!m_isWaitingForDish)
+        {
+            return false;
+        }
+
+        if (dishType == desiredDishType)
+        {
+            LeaveTip((uint)Random.Range(10, 20));//todo
+        }
+
+        StartCoroutine(PostServeCoroutine());
+        return true;
+    }
+
+    IEnumerator PostServeCoroutine()
+    {
+        m_isWaitingForDish = false;
+        yield return new WaitForSeconds(1.0f);
+        desiredDishType = (uint)Random.Range(0, dishTypeCount - 1);
+        m_isWaitingForDish = true;
+    }
+
+    void OnGUI()
+    {
+        if (isWaitingForDish)  // or check the app debug flag
+        {
+            Transform transform = GetComponent<Transform>();
+            Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+            GUIStyle style = new GUIStyle();
+            style.normal.textColor = Color.black;
+            GUI.Label(new Rect(pos.x, Screen.height - pos.y, 100, 100), "I want dish type <" + desiredDishType + ">", style);
+        }
+    }
+}
---
+++ Assets/Scripts/TableTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableTest : MonoBehaviour
+{
+	void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var tables = GameObject.FindGameObjectsWithTag("Table");
+            var table = tables[Random.Range(0, tables.Length - 1)];
+
+            table.GetComponent<Table>().Serve(0);
+        }
+	}
+}
