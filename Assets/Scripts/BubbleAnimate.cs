f9c5823a4f2004ea87bef22a040310c1e09c8f54
Marcin Kędzierski <mkedzierski@q-loc.com>
2018-07-21 21:10:50 +0200

    Add B button support

--- Assets/Scripts/BubbleAnimate.cs
+++ Assets/Scripts/BubbleAnimate.cs
@@ -3,15 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BubbleAnimate : MonoBehaviour {
-    private float fade_in = 0.1f;
-    private float fade_out = 0.3f;
-
-
-    public float duration;
-    private float passed_time;
 	// Use this for initialization
 	void Start () {
-        passed_time = 0;
 	}
 	
 	// Update is called once per frame
