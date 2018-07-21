f6a134007abb361e66e1dfe166efa391d83a3270
BigDzik0 <bigdzik0@gmail.com>
2018-07-21 14:36:22 +0200

    fixed main; working on audio;

--- Assets/Scripts/AudioScript.cs
+++ Assets/Scripts/AudioScript.cs
@@ -1,16 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioScript : MonoBehaviour {
+
+    public AudioMixerSnapshot MenuTheme;
+    public AudioMixerSnapshot GameTheme;
+    public AudioMixer mixer;
+    public AudioClip menu;
+    public AudioClip game;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void TransitionToGame()
+    {
+    }
+
+    public void TransitionToMenu()
+    {
+
+    }
+
 }
