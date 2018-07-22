ab14c9b33e983631e0fd17766c2c936f21314459
Andreas Vennström <andreas.vennstrom@gmail.com>
2018-07-22 11:20:55 +0200

    apply regular movement force in FixedUpdate instead of Update

--- Assets/Scripts/WaitersInput.cs
+++ Assets/Scripts/WaitersInput.cs
@@ -5,39 +5,52 @@ using UnityEngine;
 public class WaitersInput : MonoBehaviour {
     public float deadzone = 1.0f;
 	public Waiter[] waiters;
-    
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
+	private void Update()
+    {
 		for (int i = 0; i < waiters.Length; i++)
 		{
 			ProcessInputForWaiter(i);
 		}
 	}
-   
 
-	void ProcessInputForWaiter(int waiter_index)
-	{
-		var current_waiter = waiters[waiter_index];
-		string vertical_input_name = "Vertical" + waiter_index.ToString();
-		string horizontal_input_name = "Horizontal" + waiter_index.ToString();
-        string dash_input_name = "Dash" + waiter_index.ToString();
-        string dish_action_name = "DishAction" + waiter_index.ToString();
-        string keep_dish = "KeepDish" + waiter_index.ToString();
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < waiters.Length; i++)
+        {
+            ProcessMovementInputForWaiter(i);
+        }
+    }
+
+    void ProcessMovementInputForWaiter(int waiter_index)
+    {
+        var current_waiter = waiters[waiter_index];
+        string vertical_input_name = "Vertical" + waiter_index.ToString();
+        string horizontal_input_name = "Horizontal" + waiter_index.ToString();
         float vertical_input = Input.GetAxisRaw(vertical_input_name);
-		float horizontal_input = Input.GetAxisRaw(horizontal_input_name);
-		Vector2 input_vector = new Vector2(horizontal_input, vertical_input);
+        float horizontal_input = Input.GetAxisRaw(horizontal_input_name);
+        Vector2 input_vector = new Vector2(horizontal_input, vertical_input);
 
         if (input_vector.magnitude <= deadzone)
         {
             input_vector = Vector2.zero;
         }
-        
+
         current_waiter.ProcessInput(input_vector.normalized);
+    }
+
+    void ProcessInputForWaiter(int waiter_index)
+	{
+		var current_waiter = waiters[waiter_index];
+        string dash_input_name = "Dash" + waiter_index.ToString();
+        string dish_action_name = "DishAction" + waiter_index.ToString();
+        string keep_dish = "KeepDish" + waiter_index.ToString();
+
+        string vertical_input_name = "Vertical" + waiter_index.ToString();
+        string horizontal_input_name = "Horizontal" + waiter_index.ToString();
+        float vertical_input = Input.GetAxisRaw(vertical_input_name);
+        float horizontal_input = Input.GetAxisRaw(horizontal_input_name);
+        Vector2 input_vector = new Vector2(horizontal_input, vertical_input);
 
         if (Input.GetButtonDown(dish_action_name))
         {
