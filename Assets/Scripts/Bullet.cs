0dfe038d3d22405c663dfd083d017819809e1242
Wysocki Patryk <wysockipawelpatryk@gmail.com>
2018-07-22 10:15:33 +0200

    fixing merge conflicts

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	Rigidbody2D body;
+	public float lifetime;
+	public float stuntime;
+	public float bulletSpeed;
+
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody2D>();
+		body.AddForce(transform.up * bulletSpeed);
+		StartCoroutine(Fly());
+	}
+	
+	IEnumerator Fly()
+	{
+		yield return new WaitForSeconds(lifetime);
+		Destroy(gameObject);
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		var potential_waiter = collision.gameObject.GetComponent<Waiter>();
+		if (potential_waiter == null) return;
+
+		potential_waiter.BeginStun(stuntime);
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Client : MonoBehaviour {
+
+	public Animation anim;
+	public int numberOfShots;
+	public float gapBetweenShots;
+	public Transform hand;
+	public Transform firePoint;
+
+	public GameObject bulletPrefab;
+
+	public void ExpressDissapointment()
+	{
+		StartCoroutine(ExpressingDissapointmentCoroutine());
+	}
+
+	IEnumerator ExpressingDissapointmentCoroutine()
+	{
+		for(int i = 0; i < numberOfShots; i++)
+		{
+			var random_rot = Quaternion.AngleAxis(Random.Range(0.0f, 360.0f), new Vector3(0, 0, -1));
+			hand.rotation = random_rot;
+			//anim.Play();
+			Fire();
+			yield return new WaitForSeconds(gapBetweenShots);
+		}
+	}
+
+	void Fire()
+	{
+		var bullet = Instantiate(bulletPrefab);
+		var hand_euler = hand.rotation.eulerAngles;
+		var bullet_direction = new Vector3(hand_euler.x, hand_euler.y, hand_euler.z + 90);
+		bullet.transform.rotation = Quaternion.Euler(bullet_direction);
+		bullet.transform.position = firePoint.position;
+	}
+
+}
